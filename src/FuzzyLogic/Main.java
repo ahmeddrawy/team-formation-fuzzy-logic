c21b5d8515ceb647e98bbf1a99319552eae63cfd
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-12-28 16:47:35 +0200

    calculating membership and risks in main

---
+++ src/FuzzyLogic/Main.java
@@ -0,0 +1,30 @@
+package FuzzyLogic;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Intiator intiator = new Intiator();
+        FuzzyArray FundingArray = intiator.getFunding();
+        FuzzyArray ExpertiseArray = intiator.getExpertise();
+        HashMap<Enum<?> ,Double> ret =  FundingArray.calculate(50);
+        System.out.println("v low "+ ret.get(Fuzzy.Funding.VERY_LOW));
+        System.out.println("low " +ret.get(Fuzzy.Funding.LOW));
+        System.out.println( "medium "+ret.get(Fuzzy.Funding.MEDIUM));
+        System.out.println("high " +ret.get(Fuzzy.Funding.HIGH));
+        HashMap<Enum<?> ,Double> ret2 =  ExpertiseArray.calculate(40);
+        System.out.println("Begineer "+ ret2.get(Fuzzy.Expertise.BEGINNER));
+        System.out.println( "Intermediate "+ret2.get(Fuzzy.Expertise.INTERMEDIATE));
+        System.out.println( "Expert "+ret2.get(Fuzzy.Expertise.EXPERT));
+        double risklow = Math.max((ret.get(Fuzzy.Funding.HIGH)) ,ret2.get(Fuzzy.Expertise.EXPERT));
+        double riskNormal =Math.max( Math.min(ret.get(Fuzzy.Funding.MEDIUM),ret2.get(Fuzzy.Expertise.INTERMEDIATE)) ,ret2.get(Fuzzy.Expertise.BEGINNER) );
+        double riskHigh = Math.max(ret.get(Fuzzy.Funding.VERY_LOW), Math.min(ret.get(Fuzzy.Funding.LOW),ret2.get(Fuzzy.Expertise.BEGINNER)));
+        System.out.println("rish low "+ risklow);
+        System.out.println("rish Normal "+ riskNormal);
+        System.out.println("risk high " + riskHigh);
+
+        // write your code here
+    }
+
+}
