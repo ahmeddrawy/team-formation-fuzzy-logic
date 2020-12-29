543ad08b9e089727888e29e764d3b4ff9ee8fb75
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-12-30 00:12:40 +0200

    intiate risk with its category values

--- src/FuzzyLogic/Intiator.java
+++ src/FuzzyLogic/Intiator.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 public class Intiator {
     FuzzyArray Funding = null;
     FuzzyArray Expertise = null;
+    FuzzyArray Risk = null;
     Intiator(){
         HashMap<Enum<?> , FuzzyCategory> funding = initFunding();
         HashMap<Enum<?> , FuzzyCategory> expertise = initExpertise();
+        HashMap<Enum<?> , FuzzyCategory> risk = initRisk();
         Funding = new FuzzyArray(4,funding);
         Expertise = new FuzzyArray(3,expertise);
+        Risk = new FuzzyArray(3 , risk);
     }
     public static HashMap<Enum<?>, FuzzyCategory> initFunding(){
         HashMap<Enum<?> , FuzzyCategory> arr =new HashMap<>();
@@ -25,6 +28,17 @@ public class Intiator {
         arr.put(Fuzzy.Expertise.INTERMEDIATE,new FuzzyCategory(3,new double[]{15,30,45}));
         arr.put(Fuzzy.Expertise.EXPERT,new FuzzyCategory(3,new double[]{30,60,60}));
         return arr;
+    }
+    public static HashMap<Enum<?>, FuzzyCategory> initRisk(){
+        HashMap<Enum<?> , FuzzyCategory> arr =new HashMap<>();
+        arr.put(Fuzzy.Risk.LOW,new FuzzyCategory(3,new double[]{50,100,100}));
+        arr.put(Fuzzy.Risk.NORMAL,new FuzzyCategory(3,new double[]{25,50,75}));
+        arr.put(Fuzzy.Risk.HIGH,new FuzzyCategory(3,new double[]{0,25,50}));
+        return arr;
+    }
+
+    public FuzzyArray getRisk() {
+        return Risk;
     }
 
     public FuzzyArray getExpertise() {
