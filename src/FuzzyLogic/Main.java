6f0a580a766c6b4f5400637e8a585a2f39aef679
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-12-30 00:13:14 +0200

    calculate the answer using centroids and membership

--- src/FuzzyLogic/Main.java
+++ src/FuzzyLogic/Main.java
@@ -17,12 +17,16 @@ public class Main {
         System.out.println("Begineer "+ ret2.get(Fuzzy.Expertise.BEGINNER));
         System.out.println( "Intermediate "+ret2.get(Fuzzy.Expertise.INTERMEDIATE));
         System.out.println( "Expert "+ret2.get(Fuzzy.Expertise.EXPERT));
+        FuzzyArray Risk = intiator.getRisk();
         double risklow = Math.max((ret.get(Fuzzy.Funding.HIGH)) ,ret2.get(Fuzzy.Expertise.EXPERT));
         double riskNormal =Math.max( Math.min(ret.get(Fuzzy.Funding.MEDIUM),ret2.get(Fuzzy.Expertise.INTERMEDIATE)) ,ret2.get(Fuzzy.Expertise.BEGINNER) );
         double riskHigh = Math.max(ret.get(Fuzzy.Funding.VERY_LOW), Math.min(ret.get(Fuzzy.Funding.LOW),ret2.get(Fuzzy.Expertise.BEGINNER)));
         System.out.println("rish low "+ risklow);
         System.out.println("rish Normal "+ riskNormal);
         System.out.println("risk high " + riskHigh);
+        double ans = (risklow *Risk.get(Fuzzy.Risk.LOW).getCentroid() + riskNormal*Risk.get(Fuzzy.Risk.NORMAL).getCentroid() + riskHigh *Risk.get(Fuzzy.Risk.HIGH).getCentroid())/
+                (risklow + riskNormal + riskHigh);
+        System.out.println("answer "+ ans);
 
         // write your code here
     }
