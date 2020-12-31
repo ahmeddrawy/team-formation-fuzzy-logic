b4990b710e0fa88f08d972be84285d67ef5b6151
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2020-12-31 15:29:53 +0200

    take input from user,determine membership

--- src/FuzzyLogic/Main.java
+++ src/FuzzyLogic/Main.java
@@ -1,19 +1,23 @@
 package FuzzyLogic;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int fund = scanner.nextInt(); // 50
+        int expertise = scanner.nextInt(); /// 40
         Intiator intiator = new Intiator();
         FuzzyArray FundingArray = intiator.getFunding();
         FuzzyArray ExpertiseArray = intiator.getExpertise();
-        HashMap<Enum<?> ,Double> ret =  FundingArray.calculate(50);
+        HashMap<Enum<?> ,Double> ret =  FundingArray.calculate(fund);
         System.out.println("v low "+ ret.get(Fuzzy.Funding.VERY_LOW));
         System.out.println("low " +ret.get(Fuzzy.Funding.LOW));
         System.out.println( "medium "+ret.get(Fuzzy.Funding.MEDIUM));
         System.out.println("high " +ret.get(Fuzzy.Funding.HIGH));
-        HashMap<Enum<?> ,Double> ret2 =  ExpertiseArray.calculate(40);
+        HashMap<Enum<?> ,Double> ret2 =  ExpertiseArray.calculate(expertise);
         System.out.println("Begineer "+ ret2.get(Fuzzy.Expertise.BEGINNER));
         System.out.println( "Intermediate "+ret2.get(Fuzzy.Expertise.INTERMEDIATE));
         System.out.println( "Expert "+ret2.get(Fuzzy.Expertise.EXPERT));
@@ -31,6 +35,15 @@ public class Main {
         System.out.println("membership in low "+riskCal.get(Fuzzy.Risk.LOW));
         System.out.println("membership in normal "+riskCal.get(Fuzzy.Risk.NORMAL));
         System.out.println("membership in high "+riskCal.get(Fuzzy.Risk.HIGH));
+        if(riskCal.get(Fuzzy.Risk.LOW) >= riskCal.get(Fuzzy.Risk.NORMAL) && riskCal.get(Fuzzy.Risk.LOW) >= riskCal.get(Fuzzy.Risk.HIGH)){
+            System.out.println("Risk is low");
+        }
+        else if(riskCal.get(Fuzzy.Risk.NORMAL) >= riskCal.get(Fuzzy.Risk.LOW) && riskCal.get(Fuzzy.Risk.NORMAL) >=riskCal.get(Fuzzy.Risk.HIGH)){
+            System.out.println("Risk is Normal");
+        }
+        else {
+            System.out.println("Risk is High");
+        }
 
         // write your code here
     }
